0251da6dd8120442eed07d4b20c5db7814e7020b
Mphathi Maapola <56199797+perayepat@users.noreply.github.com>
2022-07-11 07:58:26 +0200

    :notebook: UITextFieldDelegate Added

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,46 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet var searchTextField: UITextField!
+    var weatherForCity : String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        searchTextField.delegate = self
     }
 
-
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+    }
+    ///The textfield class is responsible for triggering these methods
+    ///With multiple texfields it depends on which text field triggered the method first
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        print(searchTextField.text!)
+        searchTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if (textField.text != ""){
+            return true
+        }else{
+            textField.placeholder = "Type a city"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        //when code is done editing
+        weatherForCity = searchTextField.text ?? ""
+        searchTextField.text = ""
+    }
 }
 
